df947410db292aa0c57638568ee87bf3eb767f9a
loex345 <maitlandeg@gmail.com>
2022-07-05 16:26:54 -0700

    MVC redo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/ 
 const MAX_WRONG_GUESSES = 4;
 const PLACE_CARD ='https://i.imgur.com/YcXGjgE.png';
-const GAME_PICS =[{img:'https://i.imgur.com/Pg2Fdbb.png', match:'false'},
+const GAME_PICS = [{img:'https://i.imgur.com/Pg2Fdbb.png', match:'false'},
 {img:'https://i.imgur.com/29fSWXu.png', match:'false'},
 {img:'https://i.imgur.com/Sv44SEP.jpg', match:'false'},
 {img:'https://i.imgur.com/GCkcUeQ.jpg', match:'false'},
@@ -10,9 +10,10 @@ const GAME_PICS =[{img:'https://i.imgur.com/Pg2Fdbb.png', match:'false'},
 {img:'https://i.imgur.com/yHFSBDq.jpg', match:'false'}
 ]
 /*----- app's state (variables) -----*/
-let cards={}; //object to record card informaton 
+let cards=[]; //object to record card informaton 
 let gameStatus; //game in progress=P game is over =O
 let currentGameScore; // records number of matches 
+let firstCard=null;
 let highScore;
 let wrongGuesses;
 /*----- cached element references -----*/ 
@@ -26,64 +27,47 @@ mainEl.addEventListener('click',renderPlayerClicks); // for user interaction
 /*----- functions -----*/
 init();
 function init() {
-      // render board and set guesses
-      allDivImgs.forEach(function(allDivImg){
-         //generate new number based on imgs card length
-      let imgId=randamNumGen(GAME_PICS.length); 
-            //render cards to board using look up structurue
-      allDivImg.setAttribute("src",GAME_PICS[imgId]['img']);
-            // hide image until clicked
-      allDivImg.style.visibility='hidden'; //to do take away the hidden files
-         });
-         // to add the placeholder
-   allDivs.forEach(function(allDiv){
-         allDiv.classList.add('placeholder');
-         console.log('I am working!')
-      });
-
-      wrongGuesses=0;
-      cards= {
-         cardsMatched:0,
-         cardsVisible:'false',
-         cardsShuffled:'method',
-
-      }
+       //shuffle cards
+       cards = getShufflePicts();
+         wrongGuesses=0;
          gameStatus=null;
          currentGameScore=0;
          highScore=0;
          
          render(); 
 }
-0
 function randamNumGen(max) {
          return Math.floor(Math.random()*max);
          }
+         //get random card array
+function getShufflePicts(){
+         let tempCards=[];
+         let cards=[];
+         GAME_PICS.forEach(function(game_pic){
+            tempCards.push(game_pic, game_pic);
+         });
+         while (tempCards.length){
+            let rIdx = randamNumGen(tempCards.length);
+            let card = tempCards.splice(rIdx, 1)[0]; 
+            cards.push(card);
+         }
+      return cards;
+}
 function render() {
-         //Render should not have function code exception media
-         renderGameImgs();
+   cards.forEach(function(card, idx){
+   const imgsbyId = document.getElementById(idx);
+   const srcLnk= (card.match || card === firstCard) ? card.img : PLACE_CARD;
+   imgsbyId.src=srcLnk;
+        });
          //to accept Userclicks 
          renderPlayerClicks();
    } 
 function renderPlayerClicks(evt) {
-    //remove class on click
-    evt.target.classList.remove('placeholder');
-    //remove hidden
-    if(evt.target.tagName==='DIV'){
-      allDivImgs.forEach(function(allDivImg){
-         allDivImg.style.visibility='visible'; //only works once
-      });
-    }
-    console.log(evt.target.tagName);
-
-    render();
+  
    }
 
-function renderGameImgs(){
-         
-   }
       
 function handleClick(evt) { //replay button
-   const trackEvt= evt.target;
      
    render();
 } 
